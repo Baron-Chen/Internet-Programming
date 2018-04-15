857a9c1d8ac1d3821fece45d8e1e9726938b0c37
Baron_Chen <527651542@qq.com>
2018-04-16 02:11:16 +0800

    Home Page & Reservation Page

---
+++ WebContent/js/index_control.js
@@ -0,0 +1,14 @@
+$(document).ready(function(){
+	$("#home").on("click", function(){
+		window.location	= 'index.html'
+	})
+	$("#reservation").on("click", function(){
+		window.location	= 'reservation.html'
+	})
+	$("#menu").on("click", function(){
+		window.location	= 'menu.html'
+	})
+	$("#mine").on("click", function(){
+		window.location	= 'mine.html'
+	})
+})
