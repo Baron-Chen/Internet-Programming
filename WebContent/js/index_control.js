f5f7eee1a44e9f78b7c5c80eb25fc7360e4f0dad
Baron_Chen <527651542@qq.com>
2018-04-19 12:53:12 +0800

    Mine & Table_control Pages

--- WebContent/js/index_control.js
+++ WebContent/js/index_control.js
@@ -5,10 +5,24 @@ $(document).ready(function(){
 	$("#reservation").on("click", function(){
 		window.location	= 'reservation.html'
 	})
+	$(".reserve").on("click", function(){
+		window.location	= 'reservation.html'
+	})
+	$(".reserve").on("click", function(){
+		window.location	= 'reservation.html'
+	})
 	$("#menu").on("click", function(){
 		window.location	= 'menu.html'
 	})
 	$("#mine").on("click", function(){
 		window.location	= 'mine.html'
 	})
+
+	$("#re_reserve").on("click", function(){
+		window.location	= 'menu.html'
+	})
+	$("#cancel").on("click", function(){
+		window.location	= 'index.html'
+	})
+
 })
---
+++ WebContent/js/my_condition.js
@@ -0,0 +1,11 @@
+$(document).ready(function(){
+	var show_info = true;
+	if(show_info) {
+		$(".my_info").show();
+		$(".my_condition").hide();
+	} else {
+		$(".my_info").hide();
+		$(".my_condition").show();
+	}
+
+})
